19f0286db08e74c60c7c92ba7efa15443faa8c41
rezarestian <114054889+restianreza@users.noreply.github.com>
2023-03-20 08:33:43 +0700

    Update AdapterFootballPlayer.java

--- app/src/main/java/com/ahmfarisi/footballplayer/AdapterFootballPlayer.java
+++ app/src/main/java/com/ahmfarisi/footballplayer/AdapterFootballPlayer.java
@@ -3,6 +3,7 @@ package com.ahmfarisi.footballplayer;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -65,7 +66,12 @@ public class AdapterFootballPlayer extends RecyclerView.Adapter<AdapterFootballP
                     pesan.setPositiveButton("Ubah", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-
+                            Intent varIntent = new Intent(ctx, UbahActivity.class);
+                            varIntent.putExtra("varId", tvID.getText().toString());
+                            varIntent.putExtra("varNama", tvNama.getText().toString());
+                            varIntent.putExtra("varNomor", tvNomor.getText().toString());
+                            varIntent.putExtra("varKlub", tvKlub.getText().toString());
+                            ctx.startActivity(varIntent);
                         }
                     });
 
